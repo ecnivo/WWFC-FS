703f5ee834d3fff0f74d9843e88acf9d311aa57e
Vince Ou <ecnivo@users.noreply.github.com>
2020-04-30 16:14:55 -0400

    Dialogflow works with the google cloud  function
    Google Assistant has basic voice / simple text functionality through Dialogflow. Dialogflow works with google cloud functions server to communicate.
    Next steps: change the interaction between GCF and dialogflow more context-passing, not text-passing

--- main.py
+++ main.py
@@ -27,28 +27,40 @@ def fulfillment(request):
     action = jsonRequest["queryResult"]["action"]
     print("Action parsed is: " + str(action))  # DEBUG
 
-    response = {"fulfillment_messages": None}
-    if (action == "welcome"):
-        # first interaction
-        response["fulfillment_messages"] = {"text": {"text": "TESTING: DO YOU HEAR THIS??"}}  # DEBUG
-    elif (action == "extrainfo"):
-        # get when the status was updated, get relevant limiting weather
-        return  # TODO REMOVE THIS
-    elif (action == "wxfull"):
+    response = {}
+    responseWords = "Yikes, something went wrong. Contact the developer for help?"  # This is the default response text
+    if action == "welcome":
+        # first interaction. Quickly get the status
+        responseWords = "Hi, the current status is: " + str(getStatus())
+        # TODO add something dealing with setting the dialogflow context to the flight status
+    elif action == "extrainfo":
+        responseWords = "The current status, " + str(
+            getStatus()) + ", means that you cannot go outside"  # TODO obviously, make this work
+    elif action == "wxfull":
         # get full weather brief
-        # yike @ needing to decode
-        return  # TODO REMOVE THIS
-    elif (action == "wxsummary"):
+        # yike @ needing to decode a METAR
+        responseWords = "The local weather at CYKF is "  # TODO get weather information from METAR, make this work?
+    elif action == "wxsummary":
         # just the "likely" limiting weather
-        return  # TODO REMOVE THIS
-    elif (action == "updatetime"):
+        responseWords = "The status is likely " + str(
+            getStatus()) + " because of"  # TODO some way to get navcanada weather
+    elif action == "updatetime":
         # time twitter updated
-        return  # TODO REMOVE THIS
+        responseWords = "The twitter was last updated at "  # TODO do a "pretty time" (ie.
     else:
         # yike, throw an error (server fault)
         abort(400)
         return
 
+    response["fulfillmentMessages"] = [{"text": {"text": [responseWords]}}]
+    response["payload"] = {"google": {"expectUserResponse": True, "richResponse": {
+        "items": [{"simpleResponse": {"textToSpeech": responseWords, "displayText": responseWords}}]}}}
     responseData = json.jsonify(response)
-    print("Response message formed: " + str(responseData))  # DEBUG
+    print("Response formed: " + str(responseData))  # DEBUG
     return responseData
+
+
+def getStatus():
+    """This uses the Twitter API to get the latest flight status update.
+    Returns a string for the current flight status"""
+    return "FLY ONLY" #TODO make this lol
