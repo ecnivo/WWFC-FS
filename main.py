acc1ca8e3398d41178dcc48e02caffd928a48fd2
Vince Ou <ecnivo@users.noreply.github.com>
2020-05-04 21:55:56 -0400

    Integration with METAR for weather updates.
    TIME ZONES ARE FUN TO DEAL WITH, THANKS FOR THE WARNING TOM SCOTT.
    
    METAR functionality added and made "full weather" work with avwx API.
    Further need to add weather summary info.
    Needs further testing of day-of and yesterday-based weather info for time calculations.

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import sys, os
 import tweepy as t
 from google.cloud import secretmanager
 from datetime import datetime, timedelta
+from random import random
+import requests
+from pytz import timezone
 
 app = Flask(__name__)
 project_id = os.environ["GCP_PROJECT"]
 
-TWEET_DEPTH = 10  # TWEET_DEPTH dictates how deep to search for tweets. Occasionally, @wwfcstatus will post non-flight status tweets, sothis is the maximum number of non-flight-status-related tweets in a row
+TWEET_DEPTH = 10  # TWEET_DEPTH dictates how deep to search for tweets. Occasionally, @wwfcstatus will post
+# non-flight status tweets, sothis is the maximum number of non-flight-status-related tweets in a row
 
+client = secretmanager.SecretManagerServiceClient()
+
+toronto = timezone("America/Toronto")
 
 @app.route('/')
 def fulfillment(request: Flask.request_class):
@@ -33,10 +40,10 @@ def fulfillment(request: Flask.request_class):
     response = {}
     speechResponse = "Yikes, something went wrong. Contact the developer for help?"  # This is the default response text
     if action == "welcome":
-        # first interaction. Quickly get the status
+        # first interaction. Quickly get the status (Done)
         flightStatus = getFlightStatus()
         updateTime = flightStatus['time']
-        if updateTime.date() == datetime.today().date():  # was updated today
+        if updateTime.date() == toronto.localize(datetime.today()).date():  # was updated today
             speechResponse = "Hi, the status is currently " + flightStatus['status']
             displayResponse = flightStatus['status']
         else:  # status not current as of today
@@ -45,6 +52,7 @@ def fulfillment(request: Flask.request_class):
             displayResponse = flightStatus['status'] + " as of " + formatDatesAndTimesTheWayIWantThem(updateTime)
 
     elif action == "extrainfo":
+        # description of the flight status (Done)
         flightStatus = getFlightStatus()['status']
         speechResponse = "The current status, " + flightStatus + ", means "
         displayResponse = flightStatus + " means: "
@@ -55,9 +63,11 @@ def fulfillment(request: Flask.request_class):
             speechResponse += "you may only fly with an instructor"
             displayResponse += "you may only fly with an instructor"
         elif flightStatus == "Circuits Only for Students":
-            speechResponse += "during the day, if you are not licensed, you can only be signed out to the circuit. If it" \
-                              " is nighttime and you are not night rated, you can only be signed out for night circuits."
-            displayResponse += "Day: Not licensed - solo circuits only. Night: no night rating - night circuits only. Any time: Licensed - no restrictions"
+            speechResponse += "during the day, if you are not licensed, you can only be signed out to the circuit. " \
+                              "If it is nighttime and you are not night rated, you can only be signed out for night" \
+                              " circuits."
+            displayResponse += "Day: Not licensed - solo circuits only. Night: no night rating - night circuits only." \
+                               " Any time: Licensed - no restrictions"
         elif flightStatus == "No Student Solo":
             speechResponse += "you may not go flying unless you are licensed."
             displayResponse += "licensed pilots only. Students must be with an instructor."
@@ -68,17 +78,38 @@ def fulfillment(request: Flask.request_class):
             speechResponse += "everybody may go flying so long as they are current."
             displayResponse += "everybody may go flying!"
 
-    elif action == "wxfull":
-        # get full weather brief
-        # yike @ needing to decode a METAR
-        speechResponse = "The local weather at CYKF is "  # TODO get weather information from METAR, make this work?
-        displayResponse = speechResponse #DEBUG change later
+        displayResponse += " For more information, call dispatch at 519-648-2213"
 
-    elif action == "wxsummary":
+    elif action == "wxfull":
+        # get full weather brief (Done)
+        weather = getWeather()
+        weatherUpdateTime = toronto.localize(datetime.strptime(weather['time']['dt'][0:18],"%Y-%m-%dT%H:%M:%S"))
+        speechResponse = "The local weather at CYKF as of " + formatDatesAndTimesTheWayIWantThem(weatherUpdateTime) \
+                         + " has winds at " + weather['wind_direction']['spoken'] + " at " + str(weather['wind_speed'][
+                             'value']) + ", visibility " + str(weather['visibility']['value']) + ", clouds " + \
+                         weather['translate']['clouds'].replace(" - Reported AGL", "") + ", temperature " + weather[
+                             'temperature']['spoken'] + " and altimeter <say-as interpret-as=\"characters\">" + \
+                         weather['altimeter']['repr'].replace("A", "") + "</say-as>"
+        if random() < 0.2:
+            speechResponse += ". <break time=\"400ms\" strength\"x-strong\"/> For more information, give London FIC a" \
+                              "call at 1-866-WX-BRIEF"
+        displayResponse = weather['sanitized']
+
+    elif action == "wxsummary": #TODO this section needs to be re-done to deal with ... everything.
         # just the "likely" limiting weather
-        speechResponse = "The status is likely " + getFlightStatus()[
-            'status'] + " because of"  # TODO some way to get navcanada weather
-        displayResponse = speechResponse
+        status = getFlightStatus()
+        weather = getWeather()
+
+        if status['time'].date() == toronto.localize(datetime.today()).date():
+            speechResponse = "The status is likely " + status['status'] + " because of " + weather[
+                'speech']
+        else:
+            speechResponse = "The limiting "
+
+        if random() < 0.4:  # sometimes
+            speechResponse += " For more up-to-date info, call dispatch at 519-648-2213."
+
+        displayResponse = weather['text']
 
     elif action == "updatetime":
         # time twitter updated
@@ -103,7 +134,8 @@ def fulfillment(request: Flask.request_class):
                                     {"items":
                                         [{"simpleResponse":
                                             {
-                                                "textToSpeech": "<prosody rate=\"fast\" pitch=\"+1st\">" + speechResponse + "</prosody>",
+                                                "textToSpeech": "<prosody rate=\"fast\" pitch=\"+1st\">" + \
+                                                                speechResponse + "</prosody>",
                                                 "displayText": displayResponse}
                                         }
                                         ]
@@ -111,19 +143,19 @@ def fulfillment(request: Flask.request_class):
                                 }
                            }
     responseData = json.jsonify(response)
-    print("Response formed: " + str(responseData))  # DEBUG
+    # print("Response formed: " + response)  # DEBUG
     return responseData
 
 
 def getFlightStatus():
     """This uses the Twitter API to get the latest flight status update.
 
-    :returns: a dictionary with 'status' for a string for the flight status,'time' for the time the tweet was created, 'text' for original tweet text"""
+    :returns: a dictionary with 'status' for a string for the flight status,'time' for the time the tweet was
+    created, 'text' for original tweet text"""
 
-    print("Received a request to contact Twitter!", file=sys.stdout)  # DEBUG
+    # print("Received a request to contact Twitter!", file=sys.stdout)  # DEBUG
 
     # Gets the API Access keys from Google Secrets
-    client = secretmanager.SecretManagerServiceClient()
     APIkeyLoc = client.secret_version_path(project_id, "TWITTER_API_KEY", "latest")
     APIsecretLoc = client.secret_version_path(project_id, "TWITTER_SECRET_KEY", "latest")
     apiKeyEncoded = client.access_secret_version(APIkeyLoc)
@@ -135,11 +167,11 @@ def getFlightStatus():
     api = t.API(auth)
     statusObjects = api.user_timeline("wwfcstatus", count=TWEET_DEPTH)
     tweets = []
+    flightStatus = None
     for s in statusObjects:
 
         stripText = s.text.replace("\r", " ").replace("\n", " ").replace("\t", " ")
         lowerText = stripText.lower()
-        flightStatus = None
         if "no" in lowerText and "fly" in lowerText:
             flightStatus = "No Fly"
         elif "dual" in lowerText:
@@ -153,25 +185,45 @@ def getFlightStatus():
         elif "no" in lowerText and "rest" in lowerText:
             flightStatus = "No Restrictions"
         else:
-            print("Skipped a non-status tweet: " + stripText)
+            # print("Skipped a non-status tweet: " + stripText) #DEBUG
+            continue
 
         if flightStatus:
-            return {'text': stripText, 'time': s.created_at, 'status': flightStatus}
+            return {'text': stripText, 'time': toronto.localize(s.created_at), 'status': flightStatus}
+
     if not flightStatus:
         return {'status': "ERROR"}  # TODO handle errors gracefully
 
 
+def getWeather():
+    """Returns limiting weather as a dictionary (from JSON) for CYKF. Uses AVWX here:
+    https://github.com/avwx-rest/avwx-api"""
+    avwxKey = client.access_secret_version(
+        client.secret_version_path(project_id, "AVWX_TOKEN", 'latest')).payload.data.decode(
+        "UTF-8")  # Gets the API key for auth
+
+    requestStr = "https://avwx.rest/api/metar/CYKF?options=translate,summary," \
+                 "speech&airport=true&reporting=true&format=json&onfail=cache"
+    weather = requests.get(requestStr, headers={"Authorization": avwxKey}).json()
+
+    print(str(weather)) #DEBUG
+    return weather
+
+
 def formatDatesAndTimesTheWayIWantThem(t: datetime):
-    """Formats the date and time in a easy-to-hear manner to the way I want! Because str-format-time is a pain in the derrière!
+    """Formats the date and time in a easy-to-hear manner to the way I want! Because str-format-time
+     is a pain in the derrière!
+     PLEASE PLEASE PLEASE make sure times are in Eastern before formatting!
     :returns: a string."""
     date = t.date()
-    today = datetime.today().date()
+    now = toronto.localize(datetime.now())
+    today = toronto.localize(datetime.today()).date()
     if date == today:  # special formatting for today
-        hoursAgo = round((datetime.now() - t).total_seconds() / 3600, ndigits=1)
+        hoursAgo = round((now - t).total_seconds() / 3600, ndigits=1)
         if hoursAgo < 1:
-            return round((datetime.now() - t).total_seconds() / 60, ndigits=1) + " minutes ago"
+            return str(int(round((now - t).total_seconds() / 60))) + " minutes ago"
         elif hoursAgo < 3:
-            return hoursAgo + " hours ago"
+            return str(hoursAgo) + " hours ago"
         else:
             return " at " + t.strftime("%H") + ":" + t.strftime("%M%p")
     elif date == (today - timedelta(days=1)):
--- testClient.py
+++ testClient.py
@@ -1,15 +1,9 @@
-import json
-import requests
+from datetime import datetime as d
 
-jsonDict = {"queryResult": {"action": "welcome"}}
+MASTERtimeString = '2020-05-04T15:00:00+00:00Z'
 
-googleURL = "https://us-central1-wwfc-fs.cloudfunctions.net/dialogflowFirebaseFulfillment"
-myIP = "http://localhost:5000"
+timeString = "2020-05-04T150000+0000"
+format = "%Y-%m-%dT%H%M%S%z"
+timeString = MASTERtimeString.replace(":","").replace("Z","")
 
-curlCommand = """curl -X POST https://us-central1-wwfc-fs.cloudfunctions.net/dialogflowFirebaseFulfillment -H "content-type:application/json" -d  '{"queryResult":{"action":"welcome"}}'"""
-
-headers = {"content-type": "application/json"}
-
-jsonData = json.dumps(jsonDict)
-response = requests.post(myIP, json=jsonDict, headers=headers)
-print(response.text)
+print(str(d.strptime(timeString, format)))
